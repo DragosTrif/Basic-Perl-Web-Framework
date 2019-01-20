aec85e00639e2597353fe9d1d0e25e104dfdaa0f
DragosTrif <drd.trif@gmail.com>
2019-01-20 11:57:02 +0200

    adding Getopt::Long

--- generate_simple_app.pl
+++ generate_simple_app.pl
@@ -1,10 +1,19 @@
 use strict;
 use warnings;
 
+use Getopt::Long;
+
 use lib 'lib';
 use SinglePageAplication;
 
-my $liteapp = SinglePageAplication->new({ name => 'test' });
+my $app_name;
+
+GetOptions( 'app_name=s' => \$app_name, )
+  or die "Could not parse options";
+
+die "please provide an app name\n" unless $app_name;
+
+my $liteapp = SinglePageAplication->new( { name => $app_name } );
 
 $liteapp->create_file_path();
 $liteapp->generate_files();
