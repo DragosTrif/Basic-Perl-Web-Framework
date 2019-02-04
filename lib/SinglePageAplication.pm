87f1dfa36bc07471bd0468a3dbb008968f322fae
DragosTrif <drd.trif@gmail.com>
2019-02-04 22:47:57 +0200

    addin plugin for authorization

--- lib/SinglePageAplication.pm
+++ lib/SinglePageAplication.pm
@@ -38,6 +38,7 @@ sub _initialize {
   $self->{class_names}->{views}       = 'main';
   $self->{class_names}->{base}        = 'BaseRenderer';
   $self->{class_names}->{psgi}        = 'app';
+  $self->{class_names}->{utils}       = 'Utils';
   $self->{ConfigTemplates}            = 'ConfigTemplates';
 }
 
@@ -45,25 +46,25 @@ sub generate_files {
   my $self = shift;
 
   my $file_name = $self->{class_names};
-   
+
   foreach my $path ( keys %{ $self->{file_path} } ) {
     my $exetension = 'pm';
-    
+
     $exetension = 'tt'
       if $path eq 'views';
-    
+
     $exetension = 'psgi'
       if $path eq 'psgi';
 
     my $file = File::Spec->catdir( $self->{file_path}->{$path},
       "$file_name->{$path}.$exetension" );
 
-    print "$file\n";
+    
     open( my $fh, '>', $file );
     print $fh $self->generate_class_code( class => $path );
     close $fh;
 
-    # # this can a be a simple sub
+    # this can a be a simple sub
     $self->_format_code_and_remove_bak_files( file => $file )
       if $path ne 'views';
 
@@ -77,7 +78,9 @@ sub create_file_path {
   my $config = $self->{file_path};
 
   foreach my $file_path ( keys %{$config} ) {
-    print "Generating dir $config->{$file_path}\n";
+    print "Generating dir $config->{$file_path}\n"
+      if $file_path ne 'utils';
+   
     make_path( $config->{$file_path} )
       if !-d $config->{$file_path};
   }
@@ -91,11 +94,13 @@ sub _generate_file_path_name {
   my $app_name       = $self->{name};
   my $base_class_dir = 'Lib';
 
+  
   my $config = {
     app     => File::Spec->catdir( $app_name, $dir ),
     modells => File::Spec->catdir( $app_name, 'Modell' ),
     views   => File::Spec->catdir( $app_name, $views_dir ),
     base    => File::Spec->catdir( $app_name, $base_class_dir ),
+    utils   => File::Spec->catdir( $app_name, $base_class_dir ),
     psgi    => $self->{name},
   };
 
@@ -107,31 +112,50 @@ sub _get_method_code {
   my %params = (@_);
 
   my $config = {
-    render_main      => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'render_main.tt'),
-    say_hello        => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'say_hello.tt'),
-    render_say_hello => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'render_say_hello.tt'),
-    dispatch         => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'dispatch.tt'),
-    _render_template => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'render_template.tt'),
-    mount            => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'mount.tt'),
-    _respond_as_josn => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, '_respond_as_josn.tt'),
-    give_me_json     => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'give_me_json.tt'),
+    render_main =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'render_main.tt' ),
+    say_hello =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'say_hello.tt' ),
+    render_say_hello => File::Spec->catdir(
+      'lib', $self->{ConfigTemplates},
+      'render_say_hello.tt'
+    ),
+    dispatch =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'dispatch.tt' ),
+    _render_template => File::Spec->catdir(
+      'lib', $self->{ConfigTemplates},
+      'render_template.tt'
+    ),
+    mount => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'mount.tt' ),
+    _respond_as_josn => File::Spec->catdir(
+      'lib', $self->{ConfigTemplates},
+      '_respond_as_josn.tt'
+    ),
+    give_me_json =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'give_me_json.tt' ),
+    load_params =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'load_params.tt' ),
+    response =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'response.tt' ),
+    load_plugins =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'load_plugins.tt' ),
   };
 
-  my $code = read_file($config->{ $params{code} });
-  
+  my $code = read_file( $config->{ $params{code} } );
+
   return $code;
 }
 
 sub generate_class_code {
   my $self   = shift;
   my %params = (@_);
-
+  
   my $class_config = {
     app => {
-      class           => $self->{name},
-      superclass      => 'BaseRenderer',
-      modell      => $self->{class_names}->{modells},
-      attributes  => [
+      class      => $self->{name},
+      superclass => 'BaseRenderer',
+      modell     => $self->{class_names}->{modells},
+      attributes => [
         {
           name    => '+views',
           default => sprintf( "'%s'", 'Templates' ),
@@ -187,17 +211,35 @@ sub generate_class_code {
         },
         {
           name => '_respond_as_josn',
-           body => $self->_get_method_code( code => '_respond_as_josn' )
+          body => $self->_get_method_code( code => '_respond_as_josn' )
         },
       ]
     },
     psgi => {
+
       #class      => 'BaseRenderer',
       controller_path => $self->{class_names}->{app},
-      methods => [
+      methods         => [
         {
           body => $self->_get_method_code( code => 'mount' )
-        }],
+        }
+      ],
+    },
+    utils => {
+      methods => [
+        {
+          name => 'load_params',
+          body => $self->_get_method_code( code => 'load_params' )
+        },
+        {
+          name => 'response',
+          body => $self->_get_method_code( code => 'response' )
+        },
+        {
+          name => 'load_plugins',
+          body => $self->_get_method_code( code => 'load_plugins' )
+        },
+      ],
     }
   };
 
@@ -211,16 +253,19 @@ sub generate_class_code {
 sub _get_class_template_input {
   my $self   = shift;
   my %params = (@_);
-
+  
   my $config = {
-    app     => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'controller.tt'),
-    modells => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'model.tt'),
-    views   => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'html.tt'),
-    base    => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'base.tt'),
-    psgi    => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'app.tt'),
+    app =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'controller.tt' ),
+    modells =>
+      File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'model.tt' ),
+    views => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'html.tt' ),
+    base  => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'base.tt' ),
+    psgi  => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'app.tt' ),
+    utils => File::Spec->catdir( 'lib', $self->{ConfigTemplates}, 'utils.tt' ),
   };
 
-  return read_file($config->{ $params{class} });
+  return read_file( $config->{ $params{class} } );
 }
 
 sub _format_code_and_remove_bak_files {
